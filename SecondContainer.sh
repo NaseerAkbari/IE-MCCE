001d0f55d11b3705ec4fdfc6a2c66cd4bdfe793a
Naseer Akbari <54404798+NaseerAkbari@users.noreply.github.com>
2023-04-03 19:52:38 +0200

    last try push

--- SecondContainer.sh
+++ SecondContainer.sh
@@ -9,8 +9,8 @@ IPINT=192.168.9.251
 IPREJECT=192.168.9.253
 IPDROP=192.168.9.254
 
+FIRSTCONTAINERIP=192.168.9.1
 SECONDTCONTAINERIP=192.168.9.2
-
 ######################################
 # Strictly no IPV6
 ######################################
@@ -39,10 +39,10 @@ echo 0 > /proc/sys/net/ipv6/conf/default/forwarding
 ######################################
 # delete all existing rules.
 #
-#iptables -F
-#iptables -t nat -F
-#iptables -t mangle -F
-#iptables -X
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -X
 
 ###################################### 
 # Rules
@@ -83,14 +83,19 @@ iptables -t nat -A PREROUTING ! -i $IFINT -d 192.168.9.0/24 -j DNAT --to-destina
 
 ### Optional: Route Incoming http-Traffic to first container
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-iptables -t nat -A PREROUTING -p tcp -i $IFWAN -d $IPWAN --dport 80 -j DNAT --to-destination $SECONDTCONTAINERIP:80
-iptables -A FORWARD -p tcp -i $IFWAN -d $SECONDTCONTAINERIP --dport 80 -o $IFINT -j ACCEPT
-iptables -A FORWARD -p tcp -o $IFWAN -s $SECONDTCONTAINERIP --sport 80 -i $IFINT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -t nat -A POSTROUTING -p tcp -o $IFWAN -s $SECONDTCONTAINERIP --sport 80 -j SNAT --to-source $IPWAN:80
+iptables -t nat -A PREROUTING -p tcp -i $IFWAN -d $IPWAN --dport 80 -j DNAT --to-destination $FIRSTCONTAINERIP:80
+iptables -A FORWARD -p tcp -i $IFWAN -d $FIRSTCONTAINERIP --dport 80 -o $IFINT -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFWAN -s $FIRSTCONTAINERIP --sport 80 -i $IFINT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -p tcp -o $IFWAN -s $FIRSTCONTAINERIP --sport 80 -j SNAT --to-source $IPWAN:80
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
+####### INCOMING TRAFFIC TO CONTAINERS ######################################
+iptables -t nat -A PREROUTING -p tcp -i $IFWAN -d $IPWAN --dport 81 -j DNAT --to-destination $SECONDTCONTAINERIP:81
+iptables -A FORWARD -p tcp -i $IFWAN -d $SECONDTCONTAINERIP --dport 81 -o $IFINT -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFWAN -s $SECONDTCONTAINERIP --sport 81 -i $IFINT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -p tcp -o $IFWAN -s $SECONDTCONTAINERIP --sport 81 -j SNAT --to-source $IPWAN:8
 ####### INCOMING TRAFFIC TO MAIN SERVER ######################################
+
+
 
 ### Accept incoming ssh to main server, but only 4 new connections per second
 iptables -A INPUT -i $IFWAN -p tcp -d $IPWAN --dport 22 -m state --state NEW -m recent --name wan_22 --update --seconds 60 --hitcount 4 -j LOG --log-level info --log-prefix wansshIN
@@ -109,17 +114,44 @@ iptables -A OUTPUT -o $IFWAN -p tcp -s $IPWAN --sport 5601 -m state --state RELA
 ######### TRAFFIC BETWEEN MAIN MACHINE AND CONTAINERS ####################
 
 ### Accept Traffic from main machine to first container
+iptables -A OUTPUT -o $IFINT -s $IPINT -d $FIRSTCONTAINERIP -j ACCEPT
+iptables -A INPUT -i $IFINT -d $IPINT -s $FIRSTCONTAINERIP -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 iptables -A OUTPUT -o $IFINT -s $IPINT -d $SECONDTCONTAINERIP -j ACCEPT
 iptables -A INPUT -i $IFINT -d $IPINT -s $SECONDTCONTAINERIP -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 
+
 ######### OUTGOING TRAFFIC FROM CONTAINERS ###############################
 
-### Let the first container reach out via TCP Port 80 (http)
-iptables -A FORWARD -p tcp -i $IFINT -s $SECONDTCONTAINERIP --dport 80 -o $IFWAN -j ACCEPT
-iptables -A FORWARD -p tcp -o $IFINT -d $SECONDTCONTAINERIP --sport 80 -i $IFWAN -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -p tcp -o $IFINT -d $SECONDTCONTAINERIP --dport 80 -i $IFWAN -j REJECT
-iptables -t nat -A POSTROUTING -p tcp -s $SECONDTCONTAINERIP --dport 80 -j SNAT --to-source $IPWAN
+### Let the first container reach out via TCP Port 80 (http) SECONDTCONTAINERIP
+iptables -A FORWARD -p tcp -i $IFINT -s $FIRSTCONTAINERIP --dport 80 -o $IFWAN -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $FIRSTCONTAINERIP --sport 80 -i $IFWAN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $FIRSTCONTAINERIP --dport 80 -i $IFWAN -j REJECT
+iptables -t nat -A POSTROUTING -p tcp -s $FIRSTCONTAINERIP --dport 80 -j SNAT --to-source $IPWAN
+
+### Let the first container reach out via TCP Port 443 (https)
+iptables -A FORWARD -p tcp -i $IFINT -s $FIRSTCONTAINERIP --dport 443 -o $IFWAN -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $FIRSTCONTAINERIP --sport 443 -i $IFWAN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $FIRSTCONTAINERIP --dport 443 -i $IFWAN -j REJECT
+iptables -t nat -A POSTROUTING -p tcp -s $FIRSTCONTAINERIP --dport 443 -j SNAT --to-source $IPWAN
+
+### Let the first container reach out via UDP Port 53 (DNS)
+iptables -A FORWARD -p udp -i $IFINT -s $FIRSTCONTAINERIP --dport 53 -o $IFWAN -j ACCEPT
+iptables -A FORWARD -p udp -o $IFINT -d $FIRSTCONTAINERIP --sport 53 -i $IFWAN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p udp -o $IFINT -d $FIRSTCONTAINERIP --dport 53 -i $IFWAN -j REJECT
+iptables -t nat -A POSTROUTING -p udp -s $FIRSTCONTAINERIP --dport 53 -j SNAT --to-source $IPWAN
+
+
+
+
+############################second
+
+### Let the first container reach out via TCP Port 80 (http) SECONDTCONTAINERIP
+iptables -A FORWARD -p tcp -i $IFINT -s $SECONDTCONTAINERIP --dport 81 -o $IFWAN -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $SECONDTCONTAINERIP --sport 81 -i $IFWAN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p tcp -o $IFINT -d $SECONDTCONTAINERIP --dport 81 -i $IFWAN -j REJECT
+iptables -t nat -A POSTROUTING -p tcp -s $SECONDTCONTAINERIP --dport 81 -j SNAT --to-source $IPWAN
 
 ### Let the first container reach out via TCP Port 443 (https)
 iptables -A FORWARD -p tcp -i $IFINT -s $SECONDTCONTAINERIP --dport 443 -o $IFWAN -j ACCEPT
@@ -134,14 +166,32 @@ iptables -A FORWARD -p udp -o $IFINT -d $SECONDTCONTAINERIP --dport 53 -i $IFWAN
 iptables -t nat -A POSTROUTING -p udp -s $SECONDTCONTAINERIP --dport 53 -j SNAT --to-source $IPWAN
 
 
+#######end SECONDTCONTAINERIP
+
+
+
 ############ CONCLUSION, BLOCK&REJECT RULES  ###############################
 
 ### Reject all other traffic from and to the first container
+iptables -A INPUT -i $IFINT -d $IPINT -s $FIRSTCONTAINERIP -j REJECT
+iptables -A OUTPUT -o $IFINT -s $IPINT -d $FIRSTCONTAINERIP -j REJECT
+
+iptables -A FORWARD -s $FIRSTCONTAINERIP -j DROP
+iptables -A FORWARD -d $FIRSTCONTAINERIP -j DROP
+
+
+
+
+
+
+
 iptables -A INPUT -i $IFINT -d $IPINT -s $SECONDTCONTAINERIP -j REJECT
 iptables -A OUTPUT -o $IFINT -s $IPINT -d $SECONDTCONTAINERIP -j REJECT
 
 iptables -A FORWARD -s $SECONDTCONTAINERIP -j DROP
 iptables -A FORWARD -d $SECONDTCONTAINERIP -j DROP
+
+
 
 # iptables -A INPUT -j LOG --log-prefix DROP
 # iptables -A OUTPUT -j LOG --log-prefix DROP
